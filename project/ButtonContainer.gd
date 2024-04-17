b21ab1a8a721bd2fdfd2849fa6f5a88f0fce2da9
anthonyj-codestuff <4565327+anthonyj-codestuff@users.noreply.github.com>
2024-04-17 18:29:28 -0500

    adding simple global state

---
+++ project/ButtonContainer.gd
@@ -0,0 +1,26 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_button_easy_pressed():
+	Game.gridSize = 3
+	get_tree().change_scene_to_file("res://gameboard.tscn")
+
+
+func _on_button_medium_pressed():
+	Game.gridSize = 4
+	get_tree().change_scene_to_file("res://gameboard.tscn")
+
+
+func _on_button_hard_pressed():
+	Game.gridSize = 5
+	get_tree().change_scene_to_file("res://gameboard.tscn")
+
+
+func _on_button_daring_pressed():
+	Game.gridSize = 6
+	get_tree().change_scene_to_file("res://gameboard.tscn")
---
+++ project/global/game.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var gridSize = 0
