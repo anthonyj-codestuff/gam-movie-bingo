b4d24b971d2064832aec7bfeca0b4d6ad6b058f2
anthonyj-codestuff <4565327+anthonyj-codestuff@users.noreply.github.com>
2024-04-17 16:57:40 -0500

    adding test assets to mess with formatting

---
+++ project/BingoGrid.gd
@@ -0,0 +1,11 @@
+extends GridContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(columns)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
