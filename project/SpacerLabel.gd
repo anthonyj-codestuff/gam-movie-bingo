af7f8ebf2acb443bc851e2ef03b7f070ddf03d68
anthonyj-codestuff <4565327+anthonyj-codestuff@users.noreply.github.com>
2024-04-28 14:59:10 -0500

    fixing label

--- project/SpacerLabel.gd
+++ project/SpacerLabel.gd
@@ -1,5 +1,8 @@
 extends Label
 
+func _ready():
+	text = "You may strike up to " + str(Game.prunesRemaining) + " tiles"
+
 func _process(delta):
 	if Game.pruneMode or Game.pruneModeDisabled:
 		text = ""
