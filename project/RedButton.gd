a979fb51ab77b6fa4598fed14384b86363e21b63
anthonyj-codestuff <4565327+anthonyj-codestuff@users.noreply.github.com>
2024-04-27 20:14:43 -0500

    fixing button text when toggled

--- project/RedButton.gd
+++ project/RedButton.gd
@@ -4,35 +4,42 @@ var knifeNormal = load("res://assets/images/button_knife.png")
 var knifeHover = load("res://assets/images/button_knife_hover.png")
 var knifeToggled = load("res://assets/images/button_knife_toggled.png")
 var knifeDisabled = load("res://assets/images/button_knife_disabled.png")
-var child:Node
+var iconChild:Node
+var marginChild:Node
 
 func setTextureSibling(node:Node):
-	child = node
+	iconChild = node
+	
+func _ready():
+	marginChild = $"../StrikesBox/LabelMargin"
 
 func _process(delta):
 	if Game.pruneModeDisabled:
 		disabled = true
-		child.set_texture(knifeDisabled)
+		iconChild.set_texture(knifeDisabled)
+		marginChild.add_theme_constant_override("margin_top", 0)
 	else:
 		disabled = false
 
 func _on_toggled(buttonPressed):
 	if buttonPressed:
 		Game.pruneMode = true
-		child.set_texture(knifeToggled)
+		iconChild.set_texture(knifeToggled)
+		marginChild.add_theme_constant_override("margin_top", 4)
 	else:
 		Game.pruneMode = false
-		child.set_texture(knifeNormal)
+		iconChild.set_texture(knifeNormal)
+		marginChild.add_theme_constant_override("margin_top", 0)
 
 func _on_mouse_entered():
 	if not disabled and not Game.pruneMode:
-		child.set_texture(knifeHover)
+		iconChild.set_texture(knifeHover)
 	elif not disabled:
-		child.set_texture(knifeToggled)
+		iconChild.set_texture(knifeToggled)
 
 func _on_mouse_exited():
 	if not disabled and not Game.pruneMode:
-		child.set_texture(knifeNormal)
+		iconChild.set_texture(knifeNormal)
 	elif Game.pruneMode:
-		child.set_texture(knifeToggled)
+		iconChild.set_texture(knifeToggled)
 		
