76d5ec939d065c652c6824a62da79805053aa455
anthonyj-codestuff <4565327+anthonyj-codestuff@users.noreply.github.com>
2024-06-30 19:44:07 -0500

    properly weighting squares by rarity

--- project/scenes/gameboard.gd
+++ project/scenes/gameboard.gd
@@ -17,12 +17,10 @@ func _ready():
 	Game.currentBoard = []
 	if length and length >= pow(gridSize, 2):
 		for i in pow(gridSize, 2):
-			var randInt = randi_range(0, length-1)
-			while Game.currentBoard.has(randInt):
-				randInt = randi_range(0, length-1)
-			var cell = gameCells[randInt]
-			Game.currentBoard.push_back(randInt)
-			var newCell = Utils.getNewBingoSquareFromData(randInt)
+			var newSquareIndex = getNewGameSquareIndex(gameCells)
+			var cell = gameCells[newSquareIndex]
+			Game.currentBoard.push_back(newSquareIndex)
+			var newCell = Utils.getNewBingoSquareFromData(newSquareIndex)
 			add_child(newCell)
 	else:
 		print("Error: No game cells found")
@@ -39,3 +37,62 @@ func _process(delta):
 func _on_back_pressed():
 	Game.resetBoard()
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func getNewGameSquareIndex(squares):
+	if Game.currentGameIgnoreRarity:
+		var newIndex = randi_range(0, squares.size()-1)
+		while Game.currentBoard.has(newIndex):
+			newIndex = randi_range(0, squares.size()-1)
+		return newIndex
+	# The current game uses rarities, pick a weighted random square
+	var vcommon = 0.511
+	var common = 0.27
+	var uncommon =  0.125
+	var rare = 0.063
+	var vrare = 0.031
+	# sort cells into rarities. Filter out cells that are already on the board/history
+	var gameCellsByRarity = catCellsByRarity(squares)
+
+	var rand_rarity = randf()
+	var rarity
+	if(rand_rarity <= vrare):
+		rarity = 5
+	elif(rand_rarity <= rare+vrare):
+		rarity = 4
+	elif(rand_rarity <= uncommon+rare+vrare):
+		rarity = 3
+	elif(rand_rarity <= common+uncommon+rare+vrare):
+		rarity = 2
+	else:
+		rarity = 1
+
+	# if the selected rarity has no entries, lower the rarity
+	while not gameCellsByRarity[rarity].size() and rarity > 1:
+		print_rich("[color=orange]No more cells of rarity %s[/color]" % rarity)
+		rarity -= 1
+	if not gameCellsByRarity[rarity].size():
+		print_rich("[color=red]ERROR: No more cells of rarity %s, returning 1[/color]" % rarity)
+		return 1
+	var newIndex = randi_range(0, gameCellsByRarity[rarity].size()-1)
+	return gameCellsByRarity[rarity][newIndex]["originalIndex"]
+
+func catCellsByRarity(squares):
+	# convert [{1, "a"}, {1, "b"} {2, "c"}]
+	# to {
+	#      "1": [{1, "a"}, {1, "b"}],
+	#      "2": [{2, "c"}]
+	#    }
+	var dictRarity = {
+		1: [],
+		2: [],
+		3: [],
+		4: [],
+		5: []
+	}
+	for index in squares.size():
+		var item = squares[index]
+		# filter out cells that already exist in the board history
+		if not Game.currentBoard.has(index):
+			item["originalIndex"] = index
+			dictRarity[int(item["rarity"])].append(item)
+	return dictRarity
